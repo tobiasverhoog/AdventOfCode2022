c3a68b9e8c591f2e313b2e8498f302cc6b8ca5f2
Tobias <tverhoog@gmail.com>
2022-12-09 15:54:12 +0100

    Day8 part1

--- src/Day8.java
+++ src/Day8.java
@@ -2,10 +2,12 @@ import java.util.List;
 
 public class Day8 {
 
-    public static void main(String[] args) {
-        List<String> rows = Utils.readFile("input/ExampleInput.txt");
+    static Integer[][] grid;
 
-        Integer[][] grid = new Integer[rows.size()][];
+    public static void main(String[] args) {
+        List<String> rows = Utils.readFile("input/InputDay8.txt");
+
+        grid = new Integer[rows.size()][];
 
         for (int i = 0; i < rows.size(); i++) {
             String row = rows.get(i);
@@ -13,9 +15,81 @@ public class Day8 {
             for (int j = 0; j < row.length(); j++) {
                 treeRow[j] = Integer.valueOf(row.substring(j, j+1));
             }
+            grid[i] = treeRow;
 
         }
 
+        int visibleTreeCounter = 0;
 
+        for (int i = 1; i < grid.length - 1; i++) {
+            for (int j = 1; j < grid[i].length - 1; j++) {
+                if (hasLineOfSightToEdge(i, j)) {
+                    visibleTreeCounter++;
+                }
+            }
+        }
+
+        int topRow = grid[0].length;
+        int bottomRow = topRow;
+        int leftRow = grid.length - 2;
+        int rightRow = leftRow;
+        int visibleTrees = visibleTreeCounter + topRow + bottomRow + leftRow + rightRow;
+        System.out.println("Answer 1: " + visibleTrees);
+
+    }
+
+    private static boolean hasLineOfSightToEdge(int x, int y) {
+
+//        check west
+        boolean isVisible = checkWest(x, y);
+        if (isVisible) return true;
+
+//        check east
+        isVisible = checkEast(x, y);
+        if (isVisible) return true;
+
+//        check north
+        isVisible = checkNorth(x, y);
+        if (isVisible) return true;
+
+//        check south
+        isVisible = checkSouth(x, y);
+        return isVisible;
+    }
+
+    private static boolean checkSouth(int x, int y) {
+        for (int i = y + 1; i < grid[y].length; i++) {
+            if (grid[y][x] <= grid[i][x]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkNorth(int x, int y) {
+        for (int i = y - 1; i >= 0 ; i--) {
+            if (grid[y][x] <= grid[i][x]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkEast(int x, int y) {
+        for (int i = x + 1; i < grid.length; i++) {
+            if (grid[y][x] <= grid[y][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkWest(int x, int y) {
+        for (int i = x - 1; i >= 0 ; i--) {
+            if (grid[y][x] <= grid[y][i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
